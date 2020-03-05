313a83d0e1dc328a457da1129f600775163660fd
Gabriel Franco <gabrielfvs91@gmail.com>
2020-03-05 18:38:17 -0300

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,9 +1,12 @@
 /*Faça um programa que receba oito números inteiros e os armazene de forma ordenada em um vetor. 
  * Ou seja, assim que lidos, os números já devem ser inseridos no vetor em uma posição que mantém a ordem crescente. 
  * Eles não podem ser inseridos no final do vetor para o vetor passar por uma ordenação.
-
 Cada vez que um número for colocado no vetor o programa deve exibir os números inseridos até então.*/
 
+int EhMaior(int [], int N2)
+{
+	return 1;
+}
 
 #include <iostream>
 using namespace std;
@@ -19,12 +22,31 @@ int main()
 		
 		if (i == 0)
 			arrayDeNumeros[i] = inputNumero;
+			
+		if (i == 1 && arrayDeNumeros[0] > inputNumero)
+		{
+			arrayDeNumeros[1] = arrayDeNumeros[0];
+			arrayDeNumeros[0] = inputNumero;
+		}
+		
 		else
 		{
 			if (arrayDeNumeros[i-1] > inputNumero)
 			{
-			arrayDeNumeros[i] = arrayDeNumeros[i-1];
-			arrayDeNumeros[i-1] = inputNumero;
+				arrayDeNumeros[i] = arrayDeNumeros[i-1];
+				arrayDeNumeros[i-1] = inputNumero;
+			}
+			else
+			{
+				for (int j = 0; j < i; j++)
+				{
+					if (arrayDeNumeros[j] > inputNumero)
+					{
+						arrayDeNumeros[j+2] = arrayDeNumeros[j+1];
+						arrayDeNumeros[j+1] = arrayDeNumeros[j];
+						arrayDeNumeros[j] = inputNumero;
+					}
+				}
 			}
 		}
 		for (int j = 0; j <= i; j++)
