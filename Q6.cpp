849f5253834344bb4210fa872e10c94b83a7e169
Gabriel Franco <gabrielfvs91@gmail.com>
2020-03-12 18:16:26 -0300

    Create Q6.cpp

---
+++ Q6.cpp
@@ -0,0 +1,35 @@
+/* De acordo com a geometria básica, podemos definir três figuras simples: 
+um círculo, um triângulo e um retângulo. A dimensão de um círculo
+ é basicamente representada pelo seu raio, um retângulo necessita de dois parâmetros, 
+ base e altura, e um triângulo é representado por três valores, um para cada um de seus lados. 
+ Munido dessas informações, pode-se calcular o perímetro e a área de qualquer uma dessas figuras 
+ citadas utilizando o método matemático correto.
+
+Faça um programa que, referente a essas 3 figuras geométricas, receba as informações de 5 figuras 
+quaisquer do usuário, sendo que cada figura será composta pelo tipo dela 
+(adotando uma nomenclatura 'C' 'R' 'T' para identificá-las) além de, de acordo com a figura indicada, 
+receber os parâmetros geométricos que as representam (suas dimensões). Por fim, exibir o nome, a área e o 
+perímetro de cada uma dessas 5 figuras fornecidas pelo usuário.
+
+Obs.: As figura devem ser identificadas por um caracter da seguinte maneira:
+
+    'C' = Círculo (parâmetro: raio)
+    'R' = Retângulo (parâmetros: base e altura)
+    'T' = Triângulo (parâmetros: 3 lados - podendo ser diferentes)
+
+Obs.: Para o valor de PI utilize a constante M_PI disponível na biblioteca cmath.
+
+Sugestão: O cálculo da área de um triângulo pode ser feito através do Teorema de Herão.*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int Raio(){}
+int Perimetro(){}
+
+int main()
+{
+	
+	
+	
+}
