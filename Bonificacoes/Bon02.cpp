c05c5b579652c93899ff927e104bfa8960905d1a
Gabriel Franco <gabrielfvs91@gmail.com>
2020-03-17 20:55:59 -0300

    Create Bon02.cpp

---
+++ Bonificacoes/Bon02.cpp
@@ -0,0 +1,37 @@
+/*Bonificação 2
+
+Um pet shop precisa controlar melhor sua agenda de banhos e tosas pois vem acontecendo muitas sobrecargas em alguns horários - 
+* mais cães agendados do que a capacidade de atendimento do pet shop. Para isso, o dono o contratou para desenvolver um sistema 
+* computadorizado que permita a ele fazer esse controle de maneira mais eficiente. Devem ser permitidos apenas 3 agendamentos 
+* por horário. Esses agendamentos podem ser banho, tosa e completo (banho + tosa). Os horários para banho ou tosa são de 45min. 
+* e completo de 90 minutos. Para cada agendamento é necessário armazenar o nome do dono, o nome do cão, a raça do cão e o 
+* telefone para contato. Os clientes podem optar por realizar seu cadastro para evitar ter que informar tudo a cada agendamento. 
+* Para isso, além das informações para agendamento, o cliente informa seu CPF e, no momento de um agendamento, o mesmo informa 
+* apenas seu CPF.O dono deve cadastrar cada tipo de atendimento com seu nome e seu valor. Ao final do dia o dono deseja saber q
+* uantos banhos, quantas tosas e quantos atendimentos completos foram feitos. Além disso, 
+* ele também deseja que o faturamento seja impresso.
+
+Implemente o código necessário, utilizando classes, para solucionar o problema apresentado.
+*/
+#include <iostream>
+using namespace std;
+
+
+class Horario{
+	private:
+		char agendamento;
+};
+
+class Dono{
+	private:
+		string nome;
+	
+	
+	
+}
+
+int main()
+{
+	
+	return 0;
+}
