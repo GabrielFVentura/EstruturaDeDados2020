71c4b13d12347f4f70d5cacb662bd81691da187a
Gabriel Franco <gabrielfvs91@gmail.com>
2020-03-18 11:33:03 -0300

    Create Q18.cpp

---
+++ Revisao/Q18.cpp
@@ -0,0 +1,20 @@
+/*Escreva um programa que aloque dinamicamente uma matriz de inteiros. As dimensões da matriz deverão ser lidas do usuário.Em seguida, escreva uma função que receba um valor e retorne 1, caso o valor esteja na matriz, ou retorne 0, no caso contrário.
+
+Entradas:
+
+Quantidade de linhas e colunas(int).
+Elementos presentes na Matriz(int).
+Elemento a ser procurado na Matriz(int).
+Saídas:
+
+1, caso o valor esteja na matriz, ou 0, no caso contrário.
+Exemplo de Entrada:
+
+3 3
+3 5 8
+1 2 9
+6 7 10
+2
+Exemplo de Saída:
+
+1*/
