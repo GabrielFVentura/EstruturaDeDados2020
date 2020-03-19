762c7414ca514299c1dcb9764bb42b939c85057e
GabrielFVentura <gabrielfvs91@gmail.com>
2020-03-18 23:14:24 -0300

    add q1

--- Pilhas e Filas/Q1.cpp
+++ Pilhas e Filas/Q1.cpp
@@ -45,17 +45,16 @@ class pilhav {
         Dado *dados;
         int tamanho;
         int posTopo;
+
     public:
         pilhav(int cap = 100);
-        //~pilhav();
+        ~pilhav();
         void empilha(Dado valor);
         Dado desempilha();
         Dado espia(); // acessa elemento do topo, mas não retira
         void depura(); // imprime os dados do vetor da pilha sem removê-los (desempilhar)
         void info();  // imprime informações da pilha (tamanho e  posição do topo)
 };
-
-int pilhav::
 
 pilhav::pilhav(int cap)
 {
@@ -64,10 +63,76 @@ pilhav::pilhav(int cap)
     capacidade = cap;
     posTopo = -1;
 }
+pilhav::~pilhav()
+{
+    while (posTopo != -1)
+        desempilha();
+    posTopo = -1;
+    delete dados;
+}
+void pilhav::empilha(Dado ValorInserido)
+{
+    dados[posTopo+1] = ValorInserido;
+    tamanho++;
+    posTopo++;
+    --capacidade;
+}
+
+Dado pilhav::desempilha()
+{
+    Dado removido = dados[posTopo];
+    tamanho--;
+    posTopo--;
+    capacidade++;
+    return removido;
+}
+
+Dado pilhav::espia()
+{
+    return dados[posTopo];
+}
+
+void pilhav::depura()
+{
+    for (int i = 0; i < tamanho; i++)
+        cout << dados[i] << endl;
+
+    cout << endl;
+}
+
+void pilhav::info()
+{
+    cout << tamanho << " " << posTopo << endl;
+}
 
 int main()
 {
-    pilhav pilha;
-    pilha.empilha(5);
+    pilhav pilha(20);
+    Dado valorInserido;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> valorInserido;
+        pilha.empilha(valorInserido);
+    }
+
+    for (int i = 0; i < 3; i++)
+        pilha.desempilha();
+
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> valorInserido;
+        pilha.empilha(valorInserido);
+    }
+
+    for (int i = 0; i < 3; i++)
+        pilha.desempilha();
+
+    cout << pilha.espia() << endl;
+
+    pilha.depura();
+
+    pilha.info();
+
     return 0;
 }
